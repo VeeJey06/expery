191b7eddebc34e406045b7d86a0edbdb75c7c766
Mani Viji <42471236+VeeJey06@users.noreply.github.com>
2021-07-03 11:06:57 +0530

    Update mahitha_xml.py

--- mahitha_xml.py
+++ mahitha_xml.py
@@ -1,5 +1,6 @@
+from typing import Sequence
 import xml.etree.ElementTree as ET
-import csv
+import pandas as pd
 
 #Full path of input xml file
 input_file = r'C:\Users\SManigan\Desktop\New Text Document.xml'
@@ -16,34 +17,55 @@ namespace = {'wd': namespace}
 rows = root.iterfind('wd:Report_Entry', namespaces=namespace)
 fields = []
 data = []
+out = {}
 for row in rows:
     columns = list(row.iter())
-    col_len = len(columns)
-    new_row = []
     for column in columns:
         if 'Report_Entry' in column.tag:
             continue
-        elif len(fields) < col_len - 1:
-            try:
-                if column.text.strip():
-                    fields.append(list(column.attrib.values())[0])
-                else:
-                    fields.append(list(column.attrib.keys())[0])
-            except IndexError:
-                fields.append(column.tag.replace('{'+namespace['wd']+'}', ''))
+        try:
+            if column.text and column.text.strip():
+                fields.append(list(column.attrib.values())[0])
+            else:
+                fields.append(list(column.attrib.keys())[0].replace('{'+namespace['wd']+'}', ''))
+        except IndexError:
+            fields.append(column.tag.replace('{'+namespace['wd']+'}', ''))
+fields = list(set(fields))
+fields.sort()
+
+rows = root.iterfind('wd:Report_Entry', namespaces=namespace)
+for row in rows:
+    columns = list(row.iter())
+    field = ''
+    record = []
+    for column in columns:
+        if 'Report_Entry' in column.tag:
+            continue
+        try:
+            if column.text and column.text.strip():
+                field = list(column.attrib.values())[0]
+            else:
+                field = list(column.attrib.keys())[0].replace('{'+namespace['wd']+'}', '')
+        except IndexError:
+            field = column.tag.replace('{'+namespace['wd']+'}', '')
         try:
             value = column.text.strip()
         except AttributeError:
-            pass
+            value = ''
         if not value:
             try:
                 value = list(column.attrib.values())[0]
             except IndexError:
                 pass
-        new_row.append(value)
-    data.append(new_row)
+        while True:
+            try:
+                record[fields.index(field)] = value
+                break
+            except:
+                record.append('')
+    data.append(record)
 
-with open(output_file, 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(fields)
-    writer.writerows(data)
+df = pd.DataFrame(data)
+df.columns = fields
+df.reindex(sorted(df.columns), axis=1)
+df.to_csv(output_file, index=False)
